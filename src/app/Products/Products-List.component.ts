6e5687c97bac652002c22a2439a014b975d226a7
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-02 18:37:25 +0530

    Learn event handling in angular and do practical

--- src/app/Products/Products-List.component.ts
+++ src/app/Products/Products-List.component.ts
@@ -9,6 +9,7 @@ export class ProductListComponent {
 
   imageWidth: number = 50;
   imageMargin: number = 2;
+  showImage: boolean = false;
 
   products: any[] = [
     {
@@ -32,4 +33,8 @@ export class ProductListComponent {
       imageUrl: "assets/images/garden_cart.png",
     },
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
