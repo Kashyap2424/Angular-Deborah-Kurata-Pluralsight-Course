aafd175bbde6be2413fa6c00bbbff6bac0a54b2e
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-03 17:21:57 +0530

    Learn and do some basice routing setup in angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./Products/Products-List.component";
@@ -19,7 +20,18 @@ import { WelcomeComponent } from "./home/welcome.component";
     ProductDetailComponent,
     WelcomeComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" },
+    ]),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
